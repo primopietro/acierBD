80991bcee7a9d764ccadef87bb9538bfe5126772
primopietro <Pietro.hyde@gmail.com>
2017-06-04 13:43:16 -0400

    Login - to be done

---
+++ Acier/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<title>Insert title here</title>
+</head>
+
+<body>
+    <?php
+	for($i=1;$i<5;++$i){
+		echo "<h" . $i . "> Test Nb. " . $i . "</h" . $i . ">";
+	}
+    require_once 'create_database_environement.php';
+    ?>
+    </body>
+
+</html>
