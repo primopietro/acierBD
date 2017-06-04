bc2b792ff1d18392601179690a898ec2106d8842
primopietro <Pietro.hyde@gmail.com>
2017-06-04 11:55:54 -0400

    Setup pt2

--- Acier/newfile.php
+++ Acier/newfile.php
@@ -4,16 +4,16 @@
 <meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
 <title>Insert title here</title>
 </head>
-<frameset>
-	<frame>
-	<frame>
-	<noframes>
-		<body>
-			<p>This page uses frames. The current browser you are using does not
-				support frames.</p>
+
+<body>
+	<p>This page uses frames. The current browser you are using does not
+		support frames.</p>
     <?php
-				?>
+	for($i=1;$i<5;++$i){
+		echo "<h" . $i . "> Test Nb. " . $i . "</h" . $i . ">";
+	}
+    
+    ?>
     </body>
-	</noframes>
-</frameset>
+
 </html>
